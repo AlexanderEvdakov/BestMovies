70acf9c4791dca8128f3883fd2dfd6dd7c1b245a
Alexander Evdakov <alexander.evdakov@9yards.at>
2019-09-13 12:42:04 +0300

    Initial Commit

---
+++ BestMoovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestMoovies
+//
+//  Created by Alexander Evdakov on 9/13/19.
+//  Copyright © 2019 Alexander Evdakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
