da06edae694b63f5e087b96dd5f5b1ab52ed6703
Alexander Evdakov <alexander.evdakov@9yards.at>
2019-09-30 14:16:08 +0300

    Delete unused references

--- BestMovies/Models/SignInModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SignInModel.swift
-//  BestMoovies
-//
-//  Created by Alexander Evdakov on 9/24/19.
-//  Copyright © 2019 Alexander Evdakov. All rights reserved.
-//
-
-import Foundation
--- BestMovies/Validation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Validation.swift
-//  BestMoovies
-//
-//  Created by Alexander Evdakov on 9/18/19.
-//  Copyright © 2019 Alexander Evdakov. All rights reserved.
-//
-
-import Foundation
